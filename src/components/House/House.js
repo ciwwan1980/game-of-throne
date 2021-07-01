73530b15efa6cb858174ff6b73ab9eca05bdeb33
Ajil Abdalqader <ajilqado@gmail.com>
2021-07-01 17:43:51 +0200

    styling-dragon-Img-for-mobile-&&-desktop

--- src/components/House/House.js
+++ src/components/House/House.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import "./index.css";
+import dragon from "../../utils/icons/dragon.png";
 
 const House = ({ house }) => {
   console.log(house, "house");
@@ -14,6 +15,9 @@ const House = ({ house }) => {
         >
           <h2>{house.name}</h2>
         </Link>
+        <div className="dragonContainer">
+          <img src={dragon} alt="" />
+        </div>
       </div>
     </div>
   );
